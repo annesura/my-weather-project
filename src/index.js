09e2e85a1fe8c18d38bf26a31458419a3d64aadb
annesura <annesura00@gmail.com>
2024-12-21 18:17:05 -0500

    Added API key and url, added JS to the city and temperature

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
-function searchCity(event) {
-   event.preventDefault();
-
-    let searchFormInput = document.querySelector("#search-input");
+function updateWeather(response) {
+    let tempteratureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchFormInput.value;
+   
+    tempteratureElement.innerHTML = Math.round(temperature);
+    cityElement.innerHTML = response.data.city;
+
+}
+
+function searchCity(city) {
+    let apiKey = "0cca9cddf1f4t4bb307e8bfo1fa213f2";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(updateWeather);
+
+}
+
+function handleSearchSubmit(event) {
+   event.preventDefault();
+    let searchInput = document.querySelector("#search-input");
+
+    searchCity(searchInput.value);
 
 }
 
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", searchCity);
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Houston");
