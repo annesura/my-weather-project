a46c4718aef219c36cdabb84ab7456b95be9c98d
annesura <annesura00@gmail.com>
2025-01-04 17:45:29 -0500

    Integrated the API to make the forecast fully functional
    Changed CSS styling on the icon

--- src/index.js
+++ src/index.js
@@ -71,20 +71,30 @@ function getForecast(city) {
     axios(apiUrl).then(displayForecast);
 }
 
+function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[date.getDay()];
+}
+
 function displayForecast(response) {
-let days = ["Mon", "Tue", "Wed", "Thu", "Fri"
-];
+
 let forecastHtml = "";
 
-days.forEach( function (day) {
+response.data.daily.forEach( function (day, index) {
+    if (index < 5) {
     forecastHtml = forecastHtml + `<div class="weather-forecast-day">
-            <div class="weather-forecast-date">${day}</div>
-            <div class="weather-forecast-icon">🌤️</div>
+            <div class="weather-forecast-date">${formatDay(day.time)}</div>
+            <div>
+            <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+            </div>
             <div class="weather-forecast-temperatures"> 
-                <div class="weather-forecast-temperature"><strong>11°</strong></div>
-                 <div class="weather-forecast-temperature">7°</div>
+                <div class="weather-forecast-temperature"><strong>${Math.round(day.temperature.maximum)}°</strong></div>
+                 <div class="weather-forecast-temperature">${Math.round(day.temperature.minimum)}°</div>
                 </div>
         </div>`;
+    }
 
 });
 
